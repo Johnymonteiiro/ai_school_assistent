3f3aa919f840ed912793585ce4cbe792eeaeee37
Johny <johnymonteiiro@gmail.com>
2024-12-10 22:57:41 -0300

    fixing the error and adding new student data

--- lib/openAI.py
+++ lib/openAI.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
