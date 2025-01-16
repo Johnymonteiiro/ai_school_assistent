b5342e737482e353c8f9fafd75e16ef4b3d4491a
Johny <johnymonteiiro@gmail.com>
2025-01-16 14:17:35 -0300

    edditing the requirements files

--- lib/openAI.py
+++ lib/openAI.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
 import os
 
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = "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"
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
--- lib/qdrant.py
+++ lib/qdrant.py
@@ -4,7 +4,7 @@ from qdrant_client.models import VectorParams, Distance
 
 qdrant_db = QdrantClient(
     url="https://1ed777cc-63bc-4ba7-9895-f838b3551f88.us-east-1-0.aws.cloud.qdrant.io:6333",
-    api_key="",
+    api_key="_FePxboPzz-wYH_vzV6tbpLVukvKyH6M9gpE3JQ7nbSpbSFSKE_W_w",
 )
 
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
