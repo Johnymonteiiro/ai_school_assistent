67b818cac1ccc18a7864b9d34f295bf568dbb491
Johny <johnymonteiiro@gmail.com>
2025-01-16 17:05:46 -0300

    edditing the requirements files

--- lib/openAI.py
+++ lib/openAI.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
+from dotenv import load_dotenv
 import os
 
-os.environ["OPENAI_API_KEY"] = "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"
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
--- lib/qdrant.py
+++ lib/qdrant.py
@@ -1,10 +1,14 @@
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 qdrant_db = QdrantClient(
-    url="https://1ed777cc-63bc-4ba7-9895-f838b3551f88.us-east-1-0.aws.cloud.qdrant.io:6333",
-    api_key="_FePxboPzz-wYH_vzV6tbpLVukvKyH6M9gpE3JQ7nbSpbSFSKE_W_w",
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
 )
 
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
